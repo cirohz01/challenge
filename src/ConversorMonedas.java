b10f688aaa1a2102aee8f949b1d8abb8cd9a5850
cirohz01 <cirohz01@gmail.com>
2023-07-20 22:33:52 -0700

    first commit

---
+++ src/ConversorMonedas.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class ConversorMonedas {
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module NuevoConversorMx {
+}
